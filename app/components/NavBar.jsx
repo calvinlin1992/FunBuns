2080741b7a62d410dc191dfbd71ebc32ff27081a
Calvin Lin <calvin.lin717@gmail.com>
2017-07-26 15:28:57 -0400

    before the merge

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -10,21 +10,22 @@ class NavigationBar extends Component {
   }
 
   render() {
-    return(
-    <Navbar fluid inverse collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand><Link to="/home">FunBuns</Link></Navbar.Brand>
-      </Navbar.Header>
-      <Nav>
-        <NavItem eventKey={1}><Link to="/men">Mens</Link></NavItem>
-        <NavItem eventKey={2}><Link to="/women">Womens</Link></NavItem>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={3}><Link to="/cart">Cart</Link></NavItem>
-        <NavItem eventKey={4}><Link to="/account">Account</Link></NavItem>
-      </Nav>
-    </Navbar>
-    )}
+    return (
+      <Navbar fluid inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand><Link to="/home">FunBuns</Link></Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1}><Link to="/men">Mens</Link></NavItem>
+          <NavItem eventKey={2}><Link to="/women">Womens</Link></NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={3}><Link to="/cart">Cart</Link></NavItem>
+          <NavItem eventKey={4}><Link to="/account">Account</Link></NavItem>
+        </Nav>
+      </Navbar>
+    )
+  }
 }
 
 // const mapStateToProps = ({ auth }) => { user: auth }
