8df63bc1ea30b48ecdaa18e7397d9978237d627c
Anthony <awatson31911@gmail.com>
2017-07-27 15:54:12 -0400

    chore(redux): Add reducer wtih action creators for product component

--- app/reducers/products.jsx
+++ app/reducers/products.jsx
@@ -1,31 +1,81 @@
 import axios from 'axios'
 
 /* --------------------- ACTIONS ------------------- */
-
 const LOAD_PRODUCTS = 'LOAD_PRODUCTS'
+const ADD_PRODUCT = 'ADD_PRODUCT'
+const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
+const EDIT_PRODUCT = 'EDIT_PRODUCT'
 
 /* ------------------ACTION-CREATORS--------------- */
 const load = products => ({ type: LOAD_PRODUCTS, products })
+const add = product => ({ type: ADD_PRODUCT, product })
+const edit = product => ({ type: EDIT_PRODUCT, product })
+const remove = id => ({ type: REMOVE_PRODUCT, id })
 
 /* --------------------- THUNKS -------------------- */
 export const loadProducts = () => {
     return (dispatch) => {
-        axios.get('api/product')
+        axios.get('api/products')
             .then((products) => {
                 dispatch(load(products))
             })
     }
 }
-/* --------------------- REDUCER ------------------- */
-export default function (state = [], action) {
-var newState = Object.assign({}, state)
 
-switch (action.type) {
-    case 'LOAD_PRODUCTS':
-        newState = state.concat(action.products)
-        break
-    default: return state
+export const addProduct = () => {
+    return (dispatch) => {
+        axios.post('api/products')
+            .then((product) => {
+                dispatch(add(product))
+            })
+    }
 }
 
-    return newState;
+export const editProduct = () => {
+    return (dispatch) => {
+        axios.put('api/products/:id')
+            .then((product) => {
+                dispatch(edit(product))
+            })
+    }
+}
+
+export const removeProduct = () => {
+    return (dispatch) => {
+        axios.delete('api/products/:id')
+            .then(() => {
+                dispatch(remove())
+            })
+    }
+}
+
+/* --------------------- REDUCER ------------------- */
+export default function(state = [], action) {
+    let newState
+
+    switch (action.type) {
+        case 'LOAD_PRODUCTS':
+            newState = action.products
+        break
+
+        case 'ADD_PRODUCT':
+            newState = [...state, action.product]
+        break
+
+        case 'EDIT_PRODUCT':
+            state.map((product) => {
+                return product.id === action.product.id ? action.product : product
+            })
+        break
+
+        case 'REMOVE_PRODUCT':
+            state.filter((product) => {
+                return product.id !== action.product.id
+            })
+        break
+
+        default: return state
+    }
+
+    return newState
 }
