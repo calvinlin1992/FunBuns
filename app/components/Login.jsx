944cf0ed6c850f074e637216663112fef64982b2
Calvin <calvin.lin717@gmail.com>
2017-07-30 11:43:15 -0400

    Attempted to add Facebook Auth but running into headers error

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,46 +1,58 @@
 import React from 'react'
-import { Button, Form, FormGroup, ControlLabel, Col, FormControl } from 'react-bootstrap'
+import { Button, wellStyles, Form, FormGroup, ControlLabel, Col, FormControl } from 'react-bootstrap'
 
-export const Login = ({ login }) => (
-  <form onSubmit={evt => {
-    evt.preventDefault()
-    login(evt.target.email.value, evt.target.password.value)
-  }}>
-    <Form horizontal>
-      <FormGroup controlId="email">
-        <Col componentClass={ControlLabel} sm={2}>
-          Email
+export const Login = ({ login, loginFB }) => (
+  <div>
+    <form onSubmit={evt => {
+      evt.preventDefault()
+      login(evt.target.email.value, evt.target.password.value)
+    }}>
+      <Form horizontal>
+        <FormGroup controlId="email">
+          <Col componentClass={ControlLabel} sm={2}>
+            Email
         </Col>
-        <Col sm={4}>
-          <FormControl name='email' type="email" placeholder="Email" />
+          <Col sm={4}>
+            <FormControl name='email' type="email" placeholder="Email" />
+          </Col>
+        </FormGroup>
+        <FormGroup controlId="password">
+          <Col componentClass={ControlLabel} sm={2}>
+            Password
         </Col>
-      </FormGroup>
-      <FormGroup controlId="password">
-        <Col componentClass={ControlLabel} sm={2}>
-          Password
-        </Col>
-        <Col sm={4}>
-          <FormControl name='password' type="password" placeholder="Password" />
-        </Col>
-      </FormGroup>
-      <FormGroup>
-        <Col smOffset={2} sm={10}>
-          <Button type="submit">
-            Sign In
+          <Col sm={4}>
+            <FormControl name='password' type="password" placeholder="Password" />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Button type="submit">
+              Sign In
           </Button>
-        </Col>
-      </FormGroup>
-    </Form>
-  </form>
+          </Col>
+        </FormGroup>
+      </Form>
+    </form>
+    <form onSubmit={evt => {
+      evt.preventDefault()
+      loginFB()
+    }}>
+      <Form>
+        <div className="well" style={wellStyles}>
+          <Button bsStyle="primary" bsSize="large" block type="submit">Facebook Login</Button>
+        </div>
+      </Form>
+    </form>
+  </div>
 )
 
-import { login } from 'APP/app/reducers/auth'
+import { login, loginFB } from 'APP/app/reducers/auth'
 import { connect } from 'react-redux'
 
 // props, sends login as a prop
 export default connect(
   state => ({}),  // mapStateToProps
-  { login },        // mapDispatchToProps
+  { login, loginFB },        // mapDispatchToProps
 )(Login)
 
 // <input name="username" />
--- app/reducers/auth.jsx
+++ app/reducers/auth.jsx
@@ -5,32 +5,42 @@ export const authenticated = user => ({
     type: AUTHENTICATED, user
 })
 
+export const loginFB = () =>
+    dispatch =>
+        axios.get('/api/auth/login/facebook')
+            .then(response => {
+                console.log(response.data)
+                const theUser = response.data
+                dispatch(authenticated(theUser))
+            })
+            .catch(failed => dispatch(authenticated(null)))
+
 export const login = (username, password) =>
-  dispatch =>
-    axios.post('/api/auth/login/local',
-      {username, password})
-      .then(() => dispatch(whoami()))
-      .catch(() => dispatch(whoami()))
+    dispatch =>
+        axios.post('/api/auth/login/local',
+            { username, password })
+            .then(() => dispatch(whoami()))
+            .catch(() => dispatch(whoami()))
 
 export const logout = () =>
-  dispatch =>
-    axios.post('/api/auth/logout')
-    .then(() => dispatch(whoami()))
-    .catch(() => dispatch(whoami()))
+    dispatch =>
+        axios.post('/api/auth/logout')
+            .then(() => dispatch(whoami()))
+            .catch(() => dispatch(whoami()))
 
 export const whoami = () =>
-  dispatch =>
-    axios.get('/api/auth/whoami')
-    .then(response => {
-        const user = response.data
-        dispatch(authenticated(user))
-    })
-    .catch(failed => dispatch(authenticated(null)))
+    dispatch =>
+        axios.get('/api/auth/whoami')
+            .then(response => {
+                const user = response.data
+                dispatch(authenticated(user))
+            })
+            .catch(failed => dispatch(authenticated(null)))
 
-const reducer = (state=null, action) => {
+const reducer = (state = null, action) => {
     switch (action.type) {
-    case AUTHENTICATED:
-        return action.user
+        case AUTHENTICATED:
+            return action.user
     }
     return state
 }
