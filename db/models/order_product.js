d4c94126cb32b64789ed2319b00b09ab84c5d5d3
copper-blue <copper-blue@users.noreply.github.com>
2017-07-31 15:10:21 -0400

    Updated order and order_product models to include total and sub_total fields.  Added a reducer for carts

--- db/models/order_product.js
+++ db/models/order_product.js
@@ -1,6 +1,20 @@
 'use strict'
+const { DECIMAL, INTEGER } = require('sequelize')
 
-module.exports = db => db.define('orders_products')
+module.exports = db => db.define('orders_products', {
+  quantity: {
+    type: INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1
+    }
+  },
+  sub_total: {
+    type: DECIMAL(10, 2),
+    allowNull: false
+  }
+})
+
 
 module.exports.associations = (OrderProduct, {Order, Product}) => {
   OrderProduct.belongsTo(Order)
